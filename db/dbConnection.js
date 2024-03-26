cc0b45f3407c8c2cd8de8fa12ea38cc6e9dd6e95
Basila Medjedovic <basilamedjedovic@gmail.com>
2024-03-26 13:19:59 +0100

    Initial

---
+++ db/dbConnection.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`Connected to MongoDB: ${conn.connection.host}`);
+  } catch (err) {
+    console.error(`Error connecting to MongoDB: ${err.message}`);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
